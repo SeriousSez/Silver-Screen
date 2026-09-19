ec74b6b5dfe998bd813a823f39d31248b12ba615
Sezgin Sahin <moyumbnm@hotmail.com>
2026-09-19 19:58:35 +0200

    Add Stage School talent recruitment

--- Assets/Scripts/Presentation/UI/EmployeeInspectorUI.cs
+++ Assets/Scripts/Presentation/UI/EmployeeInspectorUI.cs
@@ -125,7 +125,7 @@ namespace SilverScreen.Presentation.UI
             if (_roleBadgeText != null) _roleBadgeText.text = _boundEmployee.Role.ToString().ToUpper();
             if (_skillText != null) _skillText.text = $"{_boundEmployee.Skill} / 100";
             if (_moraleText != null) _moraleText.text = $"{_boundEmployee.Morale}%";
-            if (_salaryText != null) _salaryText.text = "Salary: $" + _boundEmployee.Salary.ToString("N0") + "/month";
+            if (_salaryText != null) _salaryText.text = "Monthly Salary: $" + _boundEmployee.Salary.ToString("N0") + "/month";
             if (_stateText != null) _stateText.text = _boundEmployee.CurrentState.ToString();
 
             if (_actionText != null)
